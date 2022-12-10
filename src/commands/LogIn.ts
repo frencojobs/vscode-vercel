26c745c13e6641204ec05a80914078f552f20f3a
aarondill <aaronsacks2006@gmail.com>
2022-12-09 23:48:41 -0600

    fixed login by adding config option

--- src/commands/LogIn.ts
+++ src/commands/LogIn.ts
@@ -1,10 +1,14 @@
-import { Command } from '../CommandManager'
-import { VercelManager } from '../features/VercelManager'
-
+import * as vscode from 'vscode';
+import { Command } from '../CommandManager';
+import { VercelManager } from '../features/VercelManager';
 export class LogIn implements Command {
   public readonly id = 'vscode-vercel.logIn'
   constructor(private readonly vercel: VercelManager) {}
   execute() {
-    this.vercel.logIn()
+    const config = vscode.workspace.getConfiguration('vscode-vercel');
+    const apiToken = config.get("AccessToken") as string;
+    if(apiToken)
+    this.vercel.logIn(apiToken)
+    else vscode.window.showErrorMessage('Please provide vscode-vercel.AccessToken in settings.json.');
   }
 }
