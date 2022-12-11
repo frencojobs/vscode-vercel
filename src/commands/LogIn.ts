21a7867b2c7e8d9bf3f292724a28e5d8ed8c4f7e
aarondill <aaronsacks2006@gmail.com>
2022-12-11 06:07:58 -0600

    focus on panel on login

--- src/commands/LogIn.ts
+++ src/commands/LogIn.ts
@@ -1,14 +1,27 @@
-import * as vscode from 'vscode';
-import { Command } from '../CommandManager';
-import { VercelManager } from '../features/VercelManager';
+import * as vscode from 'vscode'
+
+import { Command } from '../CommandManager'
+import { VercelManager } from '../features/VercelManager'
+
 export class LogIn implements Command {
   public readonly id = 'vscode-vercel.logIn'
   constructor(private readonly vercel: VercelManager) {}
   execute() {
-    const config = vscode.workspace.getConfiguration('vscode-vercel');
-    const apiToken = config.get("AccessToken") as string;
-    if(apiToken)
-    this.vercel.logIn(apiToken)
-    else vscode.window.showErrorMessage('Please provide vscode-vercel.AccessToken in settings.json.');
+    const config = vscode.workspace.getConfiguration('vscode-vercel')
+    const apiToken = config.get('AccessToken') as string
+    if (apiToken) {
+      this.vercel.logIn(apiToken).then(
+        (success) =>
+          success &&
+          vscode.commands.executeCommand(
+            'workbench.view.extension.vscode-vercel-sidebar'
+          )
+        // vscode.commands.executeCommand('vscode-vercel-deployments.focus')
+      )
+    } else {
+      vscode.window.showErrorMessage(
+        'Please provide vscode-vercel.AccessToken in settings.json.'
+      )
+    }
   }
 }
